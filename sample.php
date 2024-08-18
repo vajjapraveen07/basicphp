b72fccaf506ebd7174b78295f1954e552635aead
vajjapraveen07 <praveenvajja97@gmail.com>
2024-08-18 18:15:34 +0530

    added sample

---
+++ sample.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html> 
+      <head>
+            <title> sample setup </title>
+      </head>
+    <body>
+        <h3> This is my sample page</h3>
+        <p> Praveen Kumar </p>
+    </body>
+</html>
